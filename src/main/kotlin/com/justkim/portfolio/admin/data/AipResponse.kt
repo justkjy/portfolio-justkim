35476758fc69e1cc43a68887342a1457ec87180a
justkjy <mysterykjy@email.com>
2024-04-07 04:55:51 +0900

    공통 개발 - DTO

--- src/main/kotlin/com/justkim/portfolio/admin/data/AipResponse.kt
+++ src/main/kotlin/com/justkim/portfolio/admin/data/AipResponse.kt
@@ -1,8 +1,22 @@
 package com.justkim.portfolio.admin.data
 
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 
-class AipResponse<T>(status: HttpStatus) : ResponseEntity<T>(status) {
+class AipResponse<T>(status: HttpStatusCode) : ResponseEntity<T>(status) {
+    companion object{
+        fun successCreater(): ResponseEntity<Any> {
+            return ResponseEntity.ok("데이터가 저장되었습니다. ")
+        }
+
+        fun successUpdate(): ResponseEntity<Any> {
+            return ResponseEntity.ok("데이터가 수정되었습니다. ")
+        }
+
+        fun successDelete(): ResponseEntity<Any> {
+            return ResponseEntity.ok("데이터가 삭제되었습니다. ")
+        }
+    }
 
 }
